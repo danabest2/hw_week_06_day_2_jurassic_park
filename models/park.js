ad7f4c442a9953be2ceac045964fe72fa08e7f53
DANIELA <danielamiritescu@gmail.com>
2019-07-30 21:37:12 +0100

    initial commit, tests for park name, ticket and dinosaurCollection

---
+++ models/park.js
@@ -0,0 +1,8 @@
+const Park = function (name, ticketPrice) {
+  this.name = name;
+  this.ticketPrice = ticketPrice;
+  this.dinosaursCollection = [];
+}
+
+
+module.exports = Park;
---
+++ specs/park_spec.js
@@ -0,0 +1,52 @@
+const assert = require('assert');
+const Park = require('../models/park.js');
+const Dinosaur = require('../models/dinosaur.js');
+
+describe('Park', function() {
+  let park
+     let dinosaur_1
+     let dinosaur_2
+     let dinosaur_3
+     let dinosaur_4
+
+  beforeEach(function () {
+    park = new Park('Omega',25)
+         dinosaur_1 = new Dinosaur('t-rex', 'carnivore', 50);
+         dinosaur_2 = new Dinosaur('Allosaurus','herbivore', 35);
+         dinosaur_3 = new Dinosaur('Baryonyx', 'omnivore', 12);
+         dinosaur_4 = new Dinosaur('Carnotaurus', 'carnivore', 60)
+       });
+
+
+  it('should have a name', function () {
+    const actual = park.name;
+    const expected = 'Omega'
+    assert.strictEqual(actual,expected);
+  });
+
+  it('should have a ticket price', function(){
+     const actual = park.ticketPrice;
+     const expected = 25;
+     assert.strictEqual(actual,expected);
+   });
+
+  it('should have a collection of dinosaurs', function(){
+     const actual = park.dinosaursCollection.length;
+     const expected = 0;
+      assert.strictEqual(actual,expected);
+  });
+
+
+
+
+  it('should be able to add a dinosaur to its collection');
+
+  it('should be able to remove a dinosaur from its collection');
+
+  it('should be able to find the dinosaur that attracts the most visitors');
+
+  it('should be able to find all dinosaurs of a particular species');
+
+  it('should be able to remove all dinosaurs of a particular species');
+
+});
